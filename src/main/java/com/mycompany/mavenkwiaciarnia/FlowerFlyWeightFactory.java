6e69d0ee7fa9b001900968fc6cf5c78cbf6ed24b
XXX <XXX@10.0.0.11>
2016-08-26 17:16:16 +0200

    ModelComponent FlyWeightFactory

--- src/main/java/com/mycompany/mavenkwiaciarnia/FlowerFlyWeightFactory.java
+++ src/main/java/com/mycompany/mavenkwiaciarnia/FlowerFlyWeightFactory.java
@@ -5,12 +5,55 @@
  */
 package com.mycompany.mavenkwiaciarnia;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Łukasz
  */
-class FlowerFlyWeightFactory {
-    static Flower flyWeightFactory(String name,String colour){
-        return null;
+public class FlowerFlyWeightFactory {
+    private static Map<String,Flower> map = new HashMap();
+    
+    public static Flower flyWeightFactory(String name,Flower.Colour colour,Object... parameters){
+        Flower flower = null;
+        String f = name + " " + colour.toString();
+        for(int i = 0 ; i < parameters.length ; i++){
+            f += " " + parameters[i].toString();
+        }
+        flower = map.get(f);
+        if(flower == null){
+            try {
+                Class classFlower = Class.forName("com.mycompany.mavenkwiaciarnia."+name);
+                Constructor constructor = classFlower.getConstructors()[0];
+                Object[] param = new Object[constructor.getParameterCount()];
+                try {
+                    for (int i = param.length - 1, j = 0; i >= 0; i--, j++) {
+                        param[i] = parameters[parameters.length - 1 - j];
+                    }
+                } catch (Exception ex) {
+                    param[0] = colour;
+                }
+                flower = (Flower) constructor.newInstance(param);
+                map.put(flower.getDescription(),flower);
+                PriceList priceList = PriceList.getInstance();
+                Double price = priceList.getPrice(flower.getDescription());
+                if(price == null){
+                    priceList.setPrice(flower.getDescription(), 50.0);
+                }
+                return flower;
+            } catch (ClassNotFoundException ex) {
+            } catch (InstantiationException ex) {
+            } catch (IllegalAccessException ex) {
+            } catch (IllegalArgumentException ex) {
+            } catch (InvocationTargetException ex) {
+            } catch(Exception ex){
+            } 
+            return null;
+        }else{
+            return flower;
+        }
     }
 }
